75b69787865b1dbe4ae1e89ce5b4b8f2b3a090bc
Abdellhadi12 <44175418+Abdellhadi12@users.noreply.github.com>
2018-12-14 18:40:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`المهم الاخلاق :( `,"http://twitch.tv/S-F")
+client.user.setGame(` الضعيف لا يغفر، فالمغفرة شيمة القوي`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
